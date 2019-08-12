4a166e246fd87fb8a7a6c24b39c96bd3787927a2
serii <seriiburduja@gmail.com>
2019-08-12 12:31:35 +0300

    header

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,22 +1,33 @@
 $(function () {
-  /*
-      https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
-      ===========================*/
-  $("html").easeScroll({
-    frameRate: 60,
-    animationTime: 1000,
-    stepSize: 90,
-    pulseAlgorithm: 1,
-    pulseScale: 8,
-    pulseNormalize: 1,
-    accelerationDelta: 20,
-    accelerationMax: 1,
-    keyboardSupport: true,
-    arrowScroll: 50,
-    touchpadSupport: true,
-    fixedBackground: true
-  });
+	/*
+			https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
+			===========================*/
+	$("html").easeScroll({
+		frameRate: 60,
+		animationTime: 1000,
+		stepSize: 90,
+		pulseAlgorithm: 1,
+		pulseScale: 8,
+		pulseNormalize: 1,
+		accelerationDelta: 20,
+		accelerationMax: 1,
+		keyboardSupport: true,
+		arrowScroll: 50,
+		touchpadSupport: true,
+		fixedBackground: true
+	});
 
+	let sandwitch = function () {
+		$('#js-sandwitch').on('click', function () {
+			if ($(window).width() < 1100) {
+				$(this).toggleClass('sandwitch--active');
+				let mainMenu = $('#js-main-menu');
+				mainMenu.toggleClass('active');
+				mainMenu.slideToggle();
+			}
+		});
+	};
+	sandwitch();
 });
 
 
